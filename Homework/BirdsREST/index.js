31d24638dee3cf278e8bac1c51731e8391a92bda
bacelarfc <fernandacbacelar@gmail.com>
2023-02-22 13:43:58 +0100

    Completed the CRUD implementation for the Birds Api

--- Homework/BirdsREST/index.js
+++ Homework/BirdsREST/index.js
@@ -5,21 +5,47 @@ app.use(express.json());
 
 app.listen(8080); 
 
-//an array of birds 
-
+//array of birds 
 const birds = [
     { id: 1, name: 'Amazon Kingfisher', color: 'Green and bronze' },
     { id: 2, name: 'Macaw', color: 'Blue' },
     { id: 3, name: 'Jacobin', color: 'White' },
   ];
 
-//get all
+//get all birds
 app.get("/birds", (req, res) => {
     res.send({ Birds: birds });
 });
 
-//get by id 
+//get bird by id 
 app.get("/birds/:id", (req, res) => {
     const bird = birds.find((b) => b.id === Number(req.params.id));
     res.send({ Bird: bird });
 });
+
+//post - create a new bird
+//used ... spread operator to merge the new ID with the actual request body
+//spread the data of req.body object into a new object.
+app.post("/birds", (req, res) => {
+    const newBird = { id: birds.length + 1, ...req.body };
+    birds.push(newBird);
+    res.send({ bird: newBird });
+});
+
+//put - update by id
+app.put("/birds/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const birdIndex = birds.findIndex((b) => b.id === id);
+    const updatedBird = { id, ...req.body };
+    birds[birdIndex] = updatedBird;
+    res.send({ bird: updatedBird });
+});
+
+//delete by id
+app.delete("/birds/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const birdIndex = birds.findIndex(b => b.id === id);
+    birds.splice(birdIndex, 1);
+    res.send({ message: `Bird with id ${id} has been deleted` });
+});
+
