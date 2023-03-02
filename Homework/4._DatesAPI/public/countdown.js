9f5af403839ebb115069380e1efe7b30ee81480a
bacelarfc <fernandacbacelar@gmail.com>
2023-03-02 14:03:32 +0100

    Fixed intervals updates

--- Homework/4._DatesAPI/public/countdown.js
+++ Homework/4._DatesAPI/public/countdown.js
@@ -1,10 +1,12 @@
+let intervalId;
+
 function startCountdown() {
   const birthdayInput = document.getElementById('birthday');
   const countdownDiv = document.getElementById('countdown');
   const secoundCountdownDiv = document.getElementById('second-countdown');
   const minDate = new Date(birthdayInput.min);
   const maxDate = new Date(birthdayInput.max);
-  
+
   const birthday = new Date(birthdayInput.value);
   if (isNaN(birthday.getTime()) || birthday < minDate || birthday > maxDate) {
     countdownDiv.innerText = 'The date input is not valid';
@@ -18,29 +20,26 @@ function startCountdown() {
   }
   const diff = nextBirthday.getTime() - now.getTime();
   const age = now.getFullYear() - birthday.getFullYear();
+
+  //created this in order to clear the intervals, so even without refreshing the page I am able to input a new date and get the according result
   
-  const seconds = Math.floor(diff / 1000);
-  const minutes = Math.floor(seconds / 60);
-  const hours = Math.floor(minutes / 60);
-  const days = Math.floor(hours / 24);
+  clearInterval(intervalId);
 
- // Use intervals to update the countdown every second
- //needs further testing
- setInterval(() => {
-  const now = new Date();
-  const nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
-  if (nextBirthday < now) {
-    nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
-  }
-  const diff = nextBirthday.getTime() - now.getTime();
-  
-  const seconds = Math.floor(diff / 1000) % 60;
-  const minutes = Math.floor(diff / (1000 * 60)) % 60;
-  const hours = Math.floor(diff / (1000 * 60 * 60)) % 24;
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  intervalId = setInterval(() => {
+    const now = new Date();
+    const nextBirthday = new Date(now.getFullYear(), birthday.getMonth(), birthday.getDate());
+    if (nextBirthday < now) {
+      nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+    }
+    const timeDifference = nextBirthday.getTime() - now.getTime();
 
-  secoundCountdownDiv.innerHTML = `You are turning ${age} years old! &#127881`;
+    const seconds = Math.floor(timeDifference / 1000) % 60;
+    const minutes = Math.floor(timeDifference / (1000 * 60)) % 60;
+    const hours = Math.floor(timeDifference / (1000 * 60 * 60)) % 24;
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 
-  countdownDiv.innerHTML = `Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`;
-}, 1000);
+    secoundCountdownDiv.innerHTML = `You are turning ${age} years old! &#127881`;
+
+    countdownDiv.innerHTML = `Days: ${days}, Hours: ${hours}, Minutes: ${minutes}, Seconds: ${seconds}`;
+  }, 1000);
 }
